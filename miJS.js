b1424d5208c83498117b21fbf42fde96931af260
CEP <galcera@hotmail.com>
2019-01-09 13:50:21 +0100

    prova1
    he fet coses pero no estic segur de si es el que diu l'enincuat.

--- miJS.js
+++ miJS.js
@@ -1,7 +1,6 @@
-console.log("funciono");
 
-function CelebrarRespuesta(pregunta , esCorrecte){
-    document.getElementById("pregunta").innerHTML = pregunta;
+
+function CelebrarRespuesta(callback, pregunta,esCorrecte){
    if(esCorrecte){
     var respostaAleatoria = Math.floor(Math.random() * 2 );
     document.getElementById("feedback").innerHTML = respostes["celebracions"][respostaAleatoria] ;
@@ -10,6 +9,14 @@ function CelebrarRespuesta(pregunta , esCorrecte){
     var respostaAleatoria = Math.floor(Math.random() * 2);
     document.getElementById("feedback").innerHTML = respostes["fracas"][respostaAleatoria] ;
    }
-
+   setInterval(callback(pregunta,esCorrecte), 10000);
 }
-CelebrarRespuesta("asdfkjasdklf", true);
+
+function test(pregunta, esCorrecte){
+    document.getElementById("pregunta").innerHTML = pregunta;
+    if(esCorrecte){
+        document.getElementById("feedback").innerHTML = "la resposta anterior ha sigut correcte";
+       }
+}
+
+CelebrarRespuesta(test,"Soc la pregunta de prova", true);
